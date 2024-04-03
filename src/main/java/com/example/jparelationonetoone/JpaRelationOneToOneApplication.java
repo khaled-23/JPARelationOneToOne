86839684eef00776a848549bf7a50edef09f16f7
khaled alkuhaily <khaledalkuhaily@gmail.com>
2024-04-03 13:50:47 +0300

    Initial commit

---
+++ src/main/java/com/example/jparelationonetoone/JpaRelationOneToOneApplication.java
@@ -0,0 +1,13 @@
+package com.example.jparelationonetoone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaRelationOneToOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaRelationOneToOneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jparelationonetoone/JpaRelationOneToOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jparelationonetoone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaRelationOneToOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
